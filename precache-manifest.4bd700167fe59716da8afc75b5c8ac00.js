0e1708fe0f05964cd2e49e58f0b2bef6000a22ff
Yan Naung Oak <yan@Yans-MBP.lan>
2020-03-17 11:07:33 +0800

    Updates

---
+++ precache-manifest.4bd700167fe59716da8afc75b5c8ac00.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45e5a28acc7779971a1957add7b99d61",
+    "url": "/flattenthecurve/index.html"
+  },
+  {
+    "revision": "e9b0016131bf13900514",
+    "url": "/flattenthecurve/static/css/2.678de7e9.chunk.css"
+  },
+  {
+    "revision": "83dfeac482bb78fcbd1d",
+    "url": "/flattenthecurve/static/css/main.d0cc6f43.chunk.css"
+  },
+  {
+    "revision": "e9b0016131bf13900514",
+    "url": "/flattenthecurve/static/js/2.2b66f827.chunk.js"
+  },
+  {
+    "revision": "83dfeac482bb78fcbd1d",
+    "url": "/flattenthecurve/static/js/main.6da28d83.chunk.js"
+  },
+  {
+    "revision": "20bd891dc9c16d61f436",
+    "url": "/flattenthecurve/static/js/runtime~main.8a5d8a0a.js"
+  },
+  {
+    "revision": "97d6266204339226f73379042d090b3f",
+    "url": "/flattenthecurve/static/media/gnpc_commodity_exports.97d62662.csv"
+  },
+  {
+    "revision": "a770b6797b68e3f8920e473eb824bac0",
+    "url": "/flattenthecurve/static/media/loader-big.a770b679.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/flattenthecurve/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "12f0820c451bdc75f4d1ef97732bf6e8",
+    "url": "/flattenthecurve/static/media/rw-widgets.12f0820c.woff"
+  },
+  {
+    "revision": "792dcd18baf5f544aabcad1883d673c2",
+    "url": "/flattenthecurve/static/media/rw-widgets.792dcd18.svg"
+  },
+  {
+    "revision": "bc7c4a59f924cf037aad6e1f9edba366",
+    "url": "/flattenthecurve/static/media/rw-widgets.bc7c4a59.eot"
+  },
+  {
+    "revision": "eceddf474df95d8d4a7e316668c3be85",
+    "url": "/flattenthecurve/static/media/rw-widgets.eceddf47.ttf"
+  }
+]);
